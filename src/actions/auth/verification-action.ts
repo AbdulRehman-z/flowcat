cacc4bf94ff992746f8b3b2a6106d0c9b3fbc653
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-02-18 17:16:38 +0500

    feat(auth): enhance email verification flow and messages

--- src/actions/auth/verification-action.ts
+++ src/actions/auth/verification-action.ts
@@ -9,36 +9,38 @@ import { verificationTokens } from "@/db/schemas/auth-schema"
 import { eq } from "drizzle-orm"
 
 export const newVerification = async function (token: string) {
-  const existigToken = await getVerificationTokenByToken(token)
+  const existingToken = await getVerificationTokenByToken(token)
 
-  if (!existigToken) {
-    return { error: "Token does not exist!" }
+  if (!existingToken) {
+    return { error: "Token does not exist! Maybe the email had already been verified!" }
   }
 
-
-  const hasExpired = new Date(existigToken.expires) < new Date()
+  const hasExpired = new Date(existingToken.expires) < new Date()
   if (hasExpired) {
-
     return { error: "Token has expired! Retry login or signup to get new token!" }
   }
 
 
-  const existingUser = await getUserByEmail(existigToken.email)
+  const existingUser = await getUserByEmail(existingToken.email)
+  console.log(existingUser)
   if (!existingUser) {
     return { error: "Email does not exist!" }
   }
 
+  if (existingUser.emailVerified) {
+    return { success: "Email already verified!" }
+  }
 
 
   await db.update(users).set({
-    email: existigToken.email,
+    email: existingToken.email,
     emailVerified: new Date()
   }).where(eq(users.id, existingUser.id))
 
 
 
-  await db.delete(verificationTokens).where(eq(verificationTokens.id, existigToken.id!))
+  await db.delete(verificationTokens).where(eq(verificationTokens.id, existingToken.id!))
 
 
-  return { success: "Email verified!" }
+  return { success: "Email verified! Close the window anytime!" }
 }

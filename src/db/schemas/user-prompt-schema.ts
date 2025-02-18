dd04847226cfeaed60bf9c78fb23ac169f592caa
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-02-18 17:16:16 +0500

    chore(db): restructure database migrations and update schemas

--- src/db/schemas/user-prompt-schema.ts
+++ src/db/schemas/user-prompt-schema.ts
@@ -2,11 +2,13 @@ import { boolean, integer, pgTable, text, timestamp } from "drizzle-orm/pg-core"
 
 export const userPrompts = pgTable("userPrompts", {
   id: text("id").primaryKey().$defaultFn(() => crypto.randomUUID()),
-  title: text("title").notNull(),
-  content: text("content").notNull(),
+  name: text("title").notNull(),
+  prompt: text("content").notNull(),
   isDefault: boolean("is_default").default(false).notNull(),
   visibility: text("visibility").default("private").notNull(),
   hearts: integer("hearts").default(0).notNull(),
+  categories: text("categories").array().notNull(),
+  tags: text("tags").array().notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });

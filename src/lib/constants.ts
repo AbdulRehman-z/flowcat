35bd9f4120453d289dc46cba1ee27e1cb3f761a0
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-02-22 18:57:48 +0500

    chore(constants): update SELECT_CATEGORY_ITEM with color coding

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -21,75 +21,89 @@ export const AI_MODELS = [
 ]
 
 export const PROMPT_TASTES = ["Formal", "Creative", "Casual", "Professional", "Technical"]
-const jobProposalPrompts = [
-  {
-    prompt: "Craft a compelling job proposal that emphasizes my expertise in [Specific Skill/Technology] and its relevance to the client's need to [Client's Specific Need/Problem].  Highlight how my approach differs from competitors and delivers superior results.",
-    metadata: {
-      communityLikes: 150,
-      usageCount: 2500,
-    },
-  },
-  {
-    prompt: "Develop a concise job proposal for a [Job Title] position that showcases my ability to [Specific Achievable Goal] within the first [Timeframe] of employment. Quantify my successes with specific metrics wherever possible.",
-    metadata: {
-      communityLikes: 120,
-      usageCount: 1800,
-    },
-  },
-  {
-    prompt: "Write a job proposal tailored to a client who values [Specific Client Value, e.g., innovation, efficiency, cost-effectiveness].  The proposal should clearly articulate how my skills and experience directly address their priorities.",
-    metadata: {
-      communityLikes: 180,
-      usageCount: 2200,
-    },
-  },
-  {
-    prompt: "Generate a job proposal that uses a storytelling approach to illustrate my passion for [Industry/Field] and how my career trajectory has prepared me for this specific opportunity.  Include a personal anecdote that highlights a relevant skill.",
-    metadata: {
-      communityLikes: 100,
-      usageCount: 1500,
-    },
-  },
-  {
-    prompt: "Develop a job proposal that emphasizes my proactive problem-solving skills.  Describe a challenging project I overcame and the innovative solutions I implemented.  Quantify the positive impact of these solutions.",
-    metadata: {
-      communityLikes: 160,
-      usageCount: 2000,
-    },
-  },
-  {
-    prompt: "Write a job proposal that focuses on my collaborative work style.  Detail a team project where I played a key role, highlighting my contributions and how I fostered a positive team environment.",
-    metadata: {
-      communityLikes: 110,
-      usageCount: 1600,
-    },
-  },
-  {
-    prompt: "Create a job proposal that highlights my adaptability and ability to learn quickly.  Describe a situation where I had to quickly master a new skill or technology to meet a deadline or solve a problem.",
-    metadata: {
-      communityLikes: 90,
-      usageCount: 1200,
-    },
-  },
-  {
-    prompt: "Generate a job proposal that leverages data and analytics to demonstrate my effectiveness.  Use relevant statistics and metrics to showcase my past achievements and project future success.",
-    metadata: {
-      communityLikes: 170,
-      usageCount: 2100,
-    },
-  },
-  {
-    prompt: "Compose a job proposal that emphasizes my leadership skills and experience managing teams or projects.  Describe a successful project you led, outlining your strategies and the positive outcomes.",
-    metadata: {
-      communityLikes: 140,
-      usageCount: 1900,
-    },
-  },
-  {
-    prompt: "Write a job proposal that incorporates a unique value proposition that sets me apart from other candidates.  This could be a specialized skill, a unique approach, or a combination of factors that contribute to exceptional results.",
-    metadata: {
-      communityLikes: 190,
-      usageCount: 2800,
-    },
-  },
+
+export const SELECT_CATEGORY_ITEM = [
+  { value: "development", label: "Development", color: "text-blue-500" },
+  { value: "marketing", label: "Marketing", color: "text-red-500" },
+  { value: "design", label: "Design", color: "text-purple-500" },
+  { value: "productivity", label: "Productivity", color: "text-green-500" },
+  { value: "management", label: "Management", color: "text-yellow-500" },
+  { value: "health", label: "Health", color: "text-teal-500" },
+  { value: "learning", label: "Learning", color: "text-orange-500" },
 ];
+
+
+
+
+// const jobProposalPrompts = [
+//   {
+//     prompt: "Craft a compelling job proposal that emphasizes my expertise in [Specific Skill/Technology] and its relevance to the client's need to [Client's Specific Need/Problem].  Highlight how my approach differs from competitors and delivers superior results.",
+//     metadata: {
+//       communityLikes: 150,
+//       usageCount: 2500,
+//     },
+//   },
+//   {
+//     prompt: "Develop a concise job proposal for a [Job Title] position that showcases my ability to [Specific Achievable Goal] within the first [Timeframe] of employment. Quantify my successes with specific metrics wherever possible.",
+//     metadata: {
+//       communityLikes: 120,
+//       usageCount: 1800,
+//     },
+//   },
+//   {
+//     prompt: "Write a job proposal tailored to a client who values [Specific Client Value, e.g., innovation, efficiency, cost-effectiveness].  The proposal should clearly articulate how my skills and experience directly address their priorities.",
+//     metadata: {
+//       communityLikes: 180,
+//       usageCount: 2200,
+//     },
+//   },
+//   {
+//     prompt: "Generate a job proposal that uses a storytelling approach to illustrate my passion for [Industry/Field] and how my career trajectory has prepared me for this specific opportunity.  Include a personal anecdote that highlights a relevant skill.",
+//     metadata: {
+//       communityLikes: 100,
+//       usageCount: 1500,
+//     },
+//   },
+//   {
+//     prompt: "Develop a job proposal that emphasizes my proactive problem-solving skills.  Describe a challenging project I overcame and the innovative solutions I implemented.  Quantify the positive impact of these solutions.",
+//     metadata: {
+//       communityLikes: 160,
+//       usageCount: 2000,
+//     },
+//   },
+//   {
+//     prompt: "Write a job proposal that focuses on my collaborative work style.  Detail a team project where I played a key role, highlighting my contributions and how I fostered a positive team environment.",
+//     metadata: {
+//       communityLikes: 110,
+//       usageCount: 1600,
+//     },
+//   },
+//   {
+//     prompt: "Create a job proposal that highlights my adaptability and ability to learn quickly.  Describe a situation where I had to quickly master a new skill or technology to meet a deadline or solve a problem.",
+//     metadata: {
+//       communityLikes: 90,
+//       usageCount: 1200,
+//     },
+//   },
+//   {
+//     prompt: "Generate a job proposal that leverages data and analytics to demonstrate my effectiveness.  Use relevant statistics and metrics to showcase my past achievements and project future success.",
+//     metadata: {
+//       communityLikes: 170,
+//       usageCount: 2100,
+//     },
+//   },
+//   {
+//     prompt: "Compose a job proposal that emphasizes my leadership skills and experience managing teams or projects.  Describe a successful project you led, outlining your strategies and the positive outcomes.",
+//     metadata: {
+//       communityLikes: 140,
+//       usageCount: 1900,
+//     },
+//   },
+//   {
+//     prompt: "Write a job proposal that incorporates a unique value proposition that sets me apart from other candidates.  This could be a specialized skill, a unique approach, or a combination of factors that contribute to exceptional results.",
+//     metadata: {
+//       communityLikes: 190,
+//       usageCount: 2800,
+//     },
+//   },
+// ];

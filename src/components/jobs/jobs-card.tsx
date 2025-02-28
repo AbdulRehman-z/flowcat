0a4d6da3fe16db995131cd0d101434aae27e03d1
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-02-28 21:42:04 +0500

    refactor(jobs): update jobs card collection component

--- src/components/jobs/jobs-card.tsx
+++ src/components/jobs/jobs-card.tsx
@@ -4,23 +4,23 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useJobsStatusContext } from "@/contexts/jobs-status-context";
 import { useScrapeJobs } from "@/hooks/jobs/use-scrape-jobs";
+import { Job } from "@/types/jobs";
 import JobCard from "./job-card";
 import JobsSkeleton from "./jobs-skeleton";
-import { Job } from "@/types/jobs";
 
 type JobsCardProps = {
   initialData?: Job[];
 };
 
 export default function JobsCard({ initialData }: JobsCardProps) {
-  const { jobs, isFetching } = useScrapeJobs();
+  const { jobs, isLoading, isRefetching } = useScrapeJobs();
   const { setIsScraping, setJobs, jobs: savedJobs } = useJobsStatusContext();
 
   if (jobs && jobs.length > 0 && jobs !== savedJobs) {
     setJobs(jobs);
   }
 
-  setIsScraping(!!isFetching);
+  setIsScraping(!!isLoading || !!isRefetching);
 
   return (
     <Card className="min-w-full mx-auto  max-h-fit bg-background/95 shadow-none">
@@ -35,7 +35,7 @@ export default function JobsCard({ initialData }: JobsCardProps) {
       <CardContent className="p-4">
         <ScrollArea className="h-[430px] pr-4 -mr-4">
           <div className="grid gap-4">
-            {isFetching ? (
+            {isLoading ? (
               <JobsSkeleton />
             ) : (
               <JobCard savedJobs={savedJobs} />

75129ebb7a02307809568279c4b903e0c1a2cb78
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-03-19 18:05:11 +0500

    feat(ui): add numeric counter animation component

---
+++ src/components/react-countup-wrapper.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import ReactCountUp from "react-countup"
+import { useState, useEffect } from "react"
+
+type ReactCountUpWrapperProps = {
+  value: number
+}
+
+export default function ReactCountUpWrapper({ value }: ReactCountUpWrapperProps) {
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return <p>Counting...</p>
+  }
+
+  return <ReactCountUp className="font-semibold" start={0} end={value} duration={1} separator="," decimals={0} />
+}
